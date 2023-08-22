670fd7cfd797272613ccc955b3493348fa826457
BenAAndrew <benandrew89@gmail.com>
2023-08-22 16:39:17 +0200

    implement get image endpoint

--- backend/app/endpoints/images/get_image.py
+++ backend/app/endpoints/images/get_image.py
@@ -1,6 +1,9 @@
 from uuid import UUID
+import os
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Response
+
+from app.config import config
 
 router = APIRouter()
 
@@ -10,4 +13,6 @@ router = APIRouter()
     status_code=status.HTTP_200_OK,
 )
 async def get_image(file_stem: str):
-    pass
+    with open(os.path.join(config.images_dir, f"{file_stem}.jpg"), "rb") as image:
+        f = image.read()
+    return Response(content=f, media_type="image/jpg")
